ac95090dc7782ba776898276851ccadbe0e98330
Pavla Zimm <zimmermannovap@gmail.com>
2023-02-01 20:14:51 +0100

    second

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 let odstavec = document.querySelector('.odstavec');
-
 function zmenBarvu() {
     odstavec.classList.toggle('csstrida');
+}
+
+
+function zmenVelikost() {
+    odstavec.classList.toggle('csstrida2');
+}
+
+
+let prehravac = document.querySelector('.mujprehravac');
+
+function spustAudio() {
+    prehravac.play();
+}
+
+function reset() {
+    prehravac.currentTime = 0;
+ }
+
+function zastavAudio() {
+    prehravac.pause();
 }
