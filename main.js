791b0989faf71ca1b9ceb5dad5c014c920c7eb77
Pavla Zimm <zimmermannovap@gmail.com>
2023-02-02 19:56:36 +0100

    oprava

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
+
 let odstavec = document.querySelector('.odstavec');
 function zmenBarvu() {
     odstavec.classList.toggle('csstrida');
 }
 
-
 function zmenVelikost() {
     odstavec.style.fontSize = "30px";
 }
+
 
 
 let prehravac = document.querySelector('.mujprehravac');
@@ -19,12 +20,12 @@ function resetAudio() {
     prehravac.currentTime = 0;
  }
 
- function resetAudio1() {
-    prehravac.currentTime = 0,5;
+ function volumeAudio() {
+    prehravac.volume = 0.5;
  }
 
- function resetAudio1() {
-    prehravac.currentTime = 1;
+ function volumeAudio1() {
+    prehravac.volume = 1;
  }
 function zastavAudio() {
     prehravac.pause();
